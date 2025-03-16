0c0271ddd92efa0790abe1bcd0996af9a588303d
viveksondagar777 <viveksondagar777@gmail.com>
2025-03-16 22:52:44 +0530

    New feature added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     }
 
   return (
-    <div className='flex items-center justify-between py-5 font-medium'>
+    <div className='flex items-center justify-between py-2 font-medium'>
       
       <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
 
