b3e83a2a986203e72a18f51c29e4ed2fe0e36248
viveksondagar777 <viveksondagar777@gmail.com>
2025-03-25 15:25:59 +0530

    New feature added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import PlaceOrder from './pages/PlaceOrder'
-import Orders from './pages/Orders'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import SearchBar from './components/SearchBar'
-import { ToastContainer, toast } from 'react-toastify';
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import SearchBar from './components/SearchBar';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/Verify'
+import Verify from './pages/Verify';
 
 const App = () => {
+  const location = useLocation(); // Current route path check karne ke liye
+
+  // Agar current page "/login" hai to Navbar, Footer & Padding remove karna hai
+  const isLoginPage = location.pathname === '/login';
+
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <div className={`${isLoginPage ? '' : 'px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'}`}>
       <ToastContainer />
-      <Navbar />
-      <SearchBar />
+      
+      {/* Navbar & SearchBar sirf tab dikhayenge jab login page nahi hoga */}
+      {!isLoginPage && <Navbar />}
+      {!isLoginPage && <SearchBar />}
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
@@ -34,9 +42,11 @@ const App = () => {
         <Route path='/orders' element={<Orders />} />
         <Route path='/verify' element={<Verify />} />
       </Routes>
-      <Footer />
-    </div>
-  )
-}
 
-export default App
+      {/* Footer bhi sirf tab dikhayenge jab login page nahi hoga */}
+      {!isLoginPage && <Footer />}
+    </div>
+  );
+};
+
+export default App;
